9da8cb71f68807a4e2bf2a9d70a547c1c3a86fd6
Arnav <arotearnav77@gmail.com>
2026-09-24 15:19:42 +0530

    Add files via upload

---
+++ voting.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int age;
+
+    cout << "Enter Your Age:";
+    cin  >> age;
+ 
+if (age>=18) {
+   cout << "You are Eligible for Voting."<<endl; 
+ } else {
+      cout << "Not Eligible for Voting!!!"<<endl;
+     }
+
+return 0;
+
+}
